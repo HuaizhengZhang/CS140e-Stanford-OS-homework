f83304066b305dbc9f52c82d968f804133155c64
HuaizhengZhang <huaizhen001@e.ntu.edu.sg>
2019-01-30 20:46:30 +0800

    variable shadow tuple array

---
+++ TheBook/projects/variables/src/main.rs
@@ -0,0 +1,47 @@
+//fn main () {
+//    let mut x = 5;
+//    println!("The value of x is: {}", x);
+//    x = 6;
+//    println!("The value of x is: {}", x);
+//}
+//
+
+//fn main() {
+//    let x = 5;
+//
+//    let x = x + 1;
+//
+//    let x = x * 2;
+//
+//    println!("The value of x is: {}", x);
+//}
+
+
+//fn main() {
+//    let spaces = "   ";
+//    let spaces = spaces.len();
+//    println!("{}", spaces)
+//}
+
+
+//fn main() {
+//    let tup: (i32, f64, u8) = (500, 6.4, 1);
+//}
+
+//fn main() {
+//    let tup = (500, 6.4, 1);
+//
+//    let (x, y, z) = tup;
+//
+//    println!("The value of y is: {}", y);
+//}
+
+fn main() {
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+}
