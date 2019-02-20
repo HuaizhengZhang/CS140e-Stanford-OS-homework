2b7484c1516afb1dac65245a9e4d858a0d40482c
HuaizhengZhang <huaizhen001@e.ntu.edu.sg>
2019-02-20 21:04:54 +0800

    can have either one mutable reference or any number of immutable references.

--- TheBook/projects/refer_borrow/src/main.rs
+++ TheBook/projects/refer_borrow/src/main.rs
@@ -1,9 +1,19 @@
 fn main () {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
 
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}", s1, len);
+
+    change(&mut s1);
+
+    println!("{}", s1);
+
+    {
+        let r1 = &mut s;
+    }
+
+    let r2 = &mut s;
 
 }
 
@@ -13,4 +23,7 @@ fn calculate_length(s: &String) -> usize {
 }
 
 
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
 
